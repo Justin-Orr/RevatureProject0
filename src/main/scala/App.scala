02f73fa9c44a01b10c5e7383e45db1ef2aeb48a3
Justin-Orr <jj.oreo98@gmail.com>
2021-07-19 13:50:32 -0500

    Process SQL query to upload book entries to MySQL server

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,19 +2,26 @@ import java.sql.DriverManager
 import java.sql.Connection
 import java.sql.SQLException
 import java.sql.SQLTimeoutException
+import java.io.FileReader
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.sql.PreparedStatement
 
 object App {
 
   var connection:Connection = null
 
   def main(args: Array[String]): Unit = {
+    //This application is a demo so the data is loaded into the database before user input is allowed for the simulation.
+
     //Test Database connection
     val connection_status = test_mysql_connection()
     if(connection_status == 1) {
       //Continue with program
-      print(Console.YELLOW)
-      print("Program Running...")
-      print(Console.RESET + "\n")
+
+      //Load the data if empty
+      //load_books_to_db()
+
       //Close the connection
       connection.close()
     }
@@ -40,7 +47,7 @@ object App {
       while ( resultSet.next() ) {
         val status = resultSet.getString("response")
         print(Console.GREEN)
-        print("Connection status = " + status)
+        print("Database Connection status = " + status)
         print(Console.RESET + "\n")
       }
 
@@ -49,9 +56,9 @@ object App {
 
     } 
     catch {
-      case a: ClassNotFoundException => a.printStackTrace(); print_connection_failure_message(); return 0;
-      case b: SQLException => b.printStackTrace(); print_connection_failure_message(); return 0;
-      case c: SQLTimeoutException => c.printStackTrace(); print_connection_failure_message(); return 0;
+      case a: ClassNotFoundException => a.printStackTrace(); print_failure_message("Connection status = Failure"); return 0;
+      case b: SQLException => b.printStackTrace(); print_failure_message("Connection status = Failure"); return 0;
+      case c: SQLTimeoutException => c.printStackTrace(); print_failure_message("Connection status = Failure"); return 0;
     } 
     finally {
       // Scala wants return statement within catch block
@@ -59,11 +66,64 @@ object App {
     
   }// End of test_mysql_connection
 
-  def print_connection_failure_message(): Unit = {
+  def print_failure_message(error_message:String): Unit = {
     print(Console.RED)
-    print("Connection status = Failure")
+    print(error_message)
     print(Console.RESET + "\n")
   }// End of print_connection_failure_message
 
+  def load_books_to_db(): Unit = {
+    var lineReader:BufferedReader = null
+    try {
+      connection.setAutoCommit(false); // To run multiple statements
+      lineReader = new BufferedReader(new FileReader("books_updated.csv"))
+      lineReader.readLine() // Skip the first line w/ the list of column names
+
+      var lineText:String = lineReader.readLine() // If the variable is null i.e. empty file then the queries wont run
+      val sql = "INSERT INTO books (title, author, genre, pages, publisher) VALUES (?, ?, ?, ?, ?)"
+      var statement:PreparedStatement = connection.prepareStatement(sql)
+
+      println("Attempting queuries...")
+
+      val batchSize = 20
+      var count = 0
+
+      while(lineText != null) {
+        var data = lineText.split(",")
+        lineText = lineReader.readLine()
+        var title = data(0)
+        var author = data(1)
+        var genre = data(2)
+        var pages = data(3).toInt
+        var publisher = data(4)
+
+        //Build statement (query)
+        statement.setString(1, title)
+        statement.setString(2, author)
+        statement.setString(3, genre)
+        statement.setInt(4, pages)
+        statement.setString(5, publisher)
+
+        statement.addBatch()
+        if(count % batchSize == 0) {
+          statement.executeBatch()
+        }
+        
+      }
+
+      lineReader.close()
+
+      // execute the remaining queries
+      statement.executeBatch();
+ 
+      connection.commit();
+
+    }
+    catch {
+      case a: FileNotFoundException => a.printStackTrace(); print_failure_message("Requested file not found.");
+      case b: SQLException => try {connection.rollback()} catch {case e: SQLException => e.printStackTrace()}
+    }
+
+  }
 
 }// EOF
